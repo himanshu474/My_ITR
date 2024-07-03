fd8d099b370c1b83bb1162653d81c2d62b13234c
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2024-07-03 15:31:06 +0000

    commit

--- ITR/src/components/Landing_page.tsx
+++ ITR/src/components/Landing_page.tsx
@@ -1,89 +1,74 @@
 
-const Landing_page = () => {
+
+const LandingPage = () => {
     return (
         <main>
-            <section className="bg-opacity-30 mt-5  py-6 pt-lg-7 pt-md-8 py-m d-6 py-sm-6 ">
+            <section className="bg-opacity-30 mt-5 py-6 pt-lg-7 pt-md-8 py-sm-6">
                 <div className="container">
                     <div className="row g-4">
                         <div className="col-md-12 text-center mb-lg-4">
-                            <h1 className="display-5 fw-bold"> India's leading Tax e-filing website </h1>
-                            <p className="text-body-secondary ms-3 ">Filing the income tax return is so straightforward that users can easily fill
-                                and submit it on their own.
-                            </p>
+                            <h1 className="display-5 fw-bold">India's leading Tax e-filing website</h1>
+                            <p className="text-body-secondary ms-3">Filing the income tax return is so straightforward that users can easily fill and submit it on their own.</p>
                         </div>
                     </div>
-                </div>
 
-                <div className="row g-4 ms-4 ">
-                    <div className="col-12 col-md-6">
-                        <div className="border-0 card-primary shadow-lg mb-5 card-lift">
-                            <div className="card-body">
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-lg-4 col-xl-4 col-md-12 col-12">
-                                        <figure className="mb-4">
-                                            <img className="img-fluid rounded-3" src="https://myitronline.com/build/assets/itr-fling-BMOhTgUc.svg" alt=" MyItrOnlineServices" />
-                                        </figure>
+                    <div className="row g-4">
+                        <div className="col-12 col-md-6 mb-4">
+                            <div className="border-0 card-primary shadow-lg mb-5 card-lift">
+                                <div className="card-body">
+                                    <div className="row d-flex align-items-center">
+                                        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                                            <figure>
+                                                <img className="img-fluid rounded-3" src="https://myitronline.com/build/assets/itr-fling-BMOhTgUc.svg" alt="MyItrOnlineServices" />
+                                            </figure>
+                                        </div>
+                                        <div className="col-lg-8 col-md-12">
+                                            <h2 className="card-title h3 mb-2">Start ITR filing Yourself</h2>
+                                            <p className="font-bold">
+                                                We have changed the way people do tax filing in India. We don’t just get your tax return filed within minutes but get you maximum refunds in the shortest time.
+                                            </p>
+                                            <a href="#" className="btn btn-warning rounded-5 me-2 icon-link icon-link-hover card-link">
+                                                File ITR Now
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                                </svg>
+                                            </a>
+                                        </div>
                                     </div>
-                                    <div className="ms-3 text-wrap col-lg-8 col-xl-8 col-md-12 col-12">
-                                        <h2 className="card-title h3 mb-2"> Start ITR filing Yourself</h2>
-                                        <p className="font-bold">
-                                            We have changed the way people do tax filing in India. We
-                                            don’t just get your tax return filed within minutes but get you maximum refunds
-                                            in shortest time.
-                                        </p>
-                                        <a href="#" className="mb-4 btn btn-warning rounded-5 me-2 icon-link icon-link-hover card-link">
-                                            File ITR Now
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </div>
+                            </div>
+                        </div>
 
-                                        </svg>
-                                        </a>
+                        <div className="col-12 col-md-6 mb-4">
+                            <div className="border-0 card-primary shadow-lg mb-5 card-lift">
+                                <div className="card-body">
+                                    <div className="row d-flex align-items-center">
+                                        <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                                            <figure>
+                                                <img className="img-fluid rounded-3" src="https://myitronline.com/build/assets/call-support-DMg_3X3d.svg" alt="MYITRONLINE SERVICES" />
+                                            </figure>
+                                        </div>
+                                        <div className="col-lg-8 col-md-12">
+                                            <h2 className="card-title h3 mb-2">Start ITR filing Yourself</h2>
+                                            <p className="font-bold">
+                                                We have changed the way people do tax filing in India. We don’t just get your tax return filed within minutes but get you maximum refunds in the shortest time.
+                                            </p>
+                                            <a href="#" className="btn btn-warning rounded-5 me-2 icon-link icon-link-hover card-link">
+                                                File ITR Now
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+                                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                                </svg>
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-                <div className="col-12 col-md-6 ms-4  g-4 ">
-                        <div className="border-0 card-primary shadow-lg card-lift">
-                            <div className="card-body">
-                                <div className="row d-flex align-items-center">
-                                    <div className="col-lg-4 col-xl-4 col-md-12 col-12">
-                                        <figure className="mb-4 ">
-                                        <img className="img-fluid rounded-3" src="https://myitronline.com/build/assets/call-support-DMg_3X3d.svg" alt="MYITRONLINE SERVICES"/>
-
-                                        </figure>
-                                    </div>
-                                    <div className="col-lg-8 col-xl-8 col-md-12 col-12 ">
-                                        <h2 className="card-title h3"> Start ITR filing Yourself</h2>
-                                        <p>
-                                            We have changed the way people do tax filing in India. We
-                                            don’t just get your tax return filed within minutes but get you maximum refunds
-                                            in shortest time.
-                                        </p>
-                                        <a href="#" className="btn btn-warning rounded-5 me-2 icon-link icon-link-hover card-link ">
-                                            File ITR Now
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                                        </svg>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-<a>
-    
-</a>
-
-
             </section>
         </main>
+    );
+};
 
-
-    )
-}
-
-
-export default Landing_page
+export default LandingPage;
