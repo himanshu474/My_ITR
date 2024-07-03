aad55c0ba759832b859af5685b1c10e2f182e495
Himanshu Rawat <60884300+himanshu474@users.noreply.github.com>
2024-07-03 18:36:27 +0000

    commit

--- ITR/src/components/Landing_page.tsx
+++ ITR/src/components/Landing_page.tsx
@@ -1,9 +1,7 @@
-
-
 const LandingPage = () => {
     return (
         <main>
-            <section className="bg-opacity-30 mt-5 py-6 pt-lg-7 pt-md-8 py-sm-6">
+            <section className="bg-opacity-30 mt-5 py-6 pt-lg-9 pt-md-8 py-sm-6">
                 <div className="container">
                     <div className="row g-4">
                         <div className="col-md-12 text-center mb-lg-4">
@@ -12,7 +10,7 @@ const LandingPage = () => {
                         </div>
                     </div>
 
-                    <div className="row g-4">
+                    <div className="row g-3">
                         <div className="col-12 col-md-6 mb-4">
                             <div className="border-0 card-primary shadow-lg mb-5 card-lift">
                                 <div className="card-body">
@@ -48,13 +46,15 @@ const LandingPage = () => {
                                                 <img className="img-fluid rounded-3" src="https://myitronline.com/build/assets/call-support-DMg_3X3d.svg" alt="MYITRONLINE SERVICES" />
                                             </figure>
                                         </div>
-                                        <div className="col-lg-8 col-md-12">
-                                            <h2 className="card-title h3 mb-2">Start ITR filing Yourself</h2>
-                                            <p className="font-bold">
-                                                We have changed the way people do tax filing in India. We don’t just get your tax return filed within minutes but get you maximum refunds in the shortest time.
+                                        <div className="col-lg-8 col-md-12 ">
+                                            <h2 className="card-title h3 mb-2 ">Hire a Tax Expert Now</h2>
+                                            <p className="font-bold ">
+                                            Entrust your taxes to our expert eCA Hire team. Benefit
+                                        from dedicated assistance, comprehensive support, total transparency, and
+                                        error-free filing.
                                             </p>
-                                            <a href="#" className="btn btn-warning rounded-5 me-2 icon-link icon-link-hover card-link">
-                                                File ITR Now
+                                            <a href="#" className="btn btn-warning rounded-5 me-2 icon-link icon-link-hover card-link ">
+                                                Hire Expert Now
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
                                                     <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
                                                 </svg>
@@ -63,6 +63,33 @@ const LandingPage = () => {
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 mb-4 mb-lg-0 text-center">
+                            <a href="#" className="btn btn-primary rounded-5 me-2 icon-link icon-link-hover">
+                                Upload Form16 Direct
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right ms-1" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="col-lg-4 col-md-6 mb-4 mb-lg-0 text-center">
+                            <a href="#" className="btn btn-primary rounded-5 me-2 icon-link icon-link-hover" >
+                                Consultancy Fee
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right ms-1" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div className="col-lg-4 mb-4 text-center">
+                            <a href="#" className="btn btn-primary rounded-5 me-2 icon-link icon-link-hover">
+                                Generate Free Rent Reciept
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-arrow-right ms-1" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
+                                </svg>
+                            </a>
                         </div>
                     </div>
                 </div>
